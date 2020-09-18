6d51b59345cfb559aaac499b1e22e68d42cb831c
huaizhan <>
2020-09-18 17:10:33 +0800

    hzkit

---
+++ Example/Pods/Target Support Files/HZViewKit/HZViewKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HZViewKit : NSObject
+@end
+@implementation PodsDummy_HZViewKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-HZViewKit_Example/Pods-HZViewKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HZViewKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HZViewKit_Example
+@end
