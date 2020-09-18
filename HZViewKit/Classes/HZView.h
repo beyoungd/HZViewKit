d599072d92cb5ccfc7984865f0df552439163aa7
huaizhan <>
2020-09-18 17:38:36 +0800

    0.1.2.0

---
+++ HZViewKit/Classes/HZView.h
@@ -0,0 +1,17 @@
+//
+//  HZView.h
+//  HZViewKit_Example
+//
+//  Created by huaizhan on 2020/9/18.
+//  Copyright © 2020 beyoungd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HZView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HZViewKit/Classes/HZView.m
@@ -0,0 +1,21 @@
+//
+//  HZView.m
+//  HZViewKit_Example
+//
+//  Created by huaizhan on 2020/9/18.
+//  Copyright © 2020 beyoungd. All rights reserved.
+//
+
+#import "HZView.h"
+
+@implementation HZView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
